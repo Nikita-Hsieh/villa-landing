4581bdab76592d4ffe36ce8c7da5edc2ef8ed358
Nikitahh <nikitahsieh03@gmail.com>
2025-02-08 23:55:39 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><div className='container bg-blue-500 mx-auto'>Villa Landing Hero Section</div></div>
+  )
+}
+
+export default App
