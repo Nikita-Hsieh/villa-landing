a0d13349a80ab8cf38a4a9f42bdd818dd5dbf133
Nikitahh <nikitahsieh03@gmail.com>
2025-02-09 23:56:29 +0800

    bg img & style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import villaImage from "./assets/villa.jpg"
 
 const App = () => {
   return (
-    <div><div className='container bg-blue-500 mx-auto'>Villa Landing Hero Section</div></div>
+    <div className="h-screen w-screen overflow-hidden relative py-5 px-10">
+      <img
+        src={villaImage}
+        alt="Background Image"
+        className="h-full w-full object-center object-cover absolute inset-0 z-0"
+      />
+      <div className="h-full w-full bg-transparent z-10 relative rounded-2xl border-[20px] border-white flex">
+        {/* main content */}
+        <div className="w-1/2 h-full flex flex-col"></div>
+        {/* side content */}
+        <div className="w-1/2 h-full"></div>
+      </div>
+    </div>
   )
 }
 
